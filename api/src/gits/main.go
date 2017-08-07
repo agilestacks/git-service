a1f73ecc4462f90bc7b695575f1270bc0705e4b1
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2017-08-07 22:32:53 +0300

    Startup blurb

--- api/src/gits/main.go
+++ api/src/gits/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"gits/api"
 	"gits/config"
 	"gits/s3"
@@ -12,5 +14,8 @@ func main() {
 	s3.Init()
 	ssh.Listen("0.0.0.0", config.SshPort)
 	api.Listen("0.0.0.0", config.HttpPort)
+	if config.Verbose {
+		log.Print("Git Service started")
+	}
 	select {}
 }
