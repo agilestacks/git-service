91dba90199149deb588e88be6aee8d8b09a831ec
Arkadi Shishlov <arkadi@agilestacks.com>
2018-11-01 21:46:58 +0200

    Extended Git status

--- api/src/gits/repo/status.go
+++ api/src/gits/repo/status.go
@@ -11,8 +11,11 @@ import (
 )
 
 type RepoStatus struct {
-	Commit string `json:"commit"`
-	Ref    string `json:"ref"`
+	Commit  string `json:"commit,omitempty"`
+	Ref     string `json:"ref,omitempty"`
+	Date    string `json:"date,omitempty"`
+	Author  string `json:"author,omitempty"`
+	Subject string `json:"subject,omitempty"`
 }
 
 func Status(repoId, ref string) (*RepoStatus, error) {
@@ -20,8 +23,11 @@ func Status(repoId, ref string) (*RepoStatus, error) {
 		ref = "master"
 	}
 	dir := filepath.Join(config.RepoDir, repoId)
+
+	gitBin := gitBinPath()
+
 	cmd := exec.Cmd{
-		Path: gitBinPath(),
+		Path: gitBin,
 		Dir:  dir,
 		Args: []string{"git", "show-ref", ref},
 	}
@@ -53,5 +59,40 @@ func Status(repoId, ref string) (*RepoStatus, error) {
 	if len(refs) > 1 {
 		return nil, fmt.Errorf("Ref `%s` refer to multiple refs %v at %v", ref, refs, commits)
 	}
-	return &RepoStatus{Commit: commits[0], Ref: refs[0]}, nil
+
+	commit := commits[0]
+	canonicalRef := refs[0]
+
+	status := &RepoStatus{Commit: commit, Ref: canonicalRef}
+
+	cmd = exec.Cmd{
+		Path: gitBin,
+		Dir:  dir,
+		Args: []string{"git", "show", "-q", "--pretty=format:%aI%n%cn <%ce>%n%s", commit}, // iso date, author, message
+	}
+	if config.Debug {
+		cmd.Stderr = os.Stdout
+	}
+	outputBytes, err = cmd.Output()
+	if len(outputBytes) == 0 {
+		return status, fmt.Errorf("Git show `%s` returned no output", commit)
+	}
+	if err != nil {
+		return status, err
+	}
+	output = string(outputBytes)
+	lines = strings.Split(output, "\n")
+	date := ""
+	if len(lines) > 0 {
+		date = lines[0]
+	}
+	author := ""
+	if len(lines) > 1 {
+		author = lines[1]
+	}
+	subject := ""
+	if len(lines) > 2 {
+		subject = lines[2]
+	}
+	return &RepoStatus{Commit: commit, Ref: canonicalRef, Author: author, Date: date, Subject: subject}, nil
 }
