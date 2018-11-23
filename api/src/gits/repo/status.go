cbae60887698dbe218de5bc23ba89fda75f23d96
Arkadi Shishlov <arkadi@agilestacks.com>
2018-11-23 02:18:31 +0200

    Status of commit sha

--- api/src/gits/repo/status.go
+++ api/src/gits/repo/status.go
@@ -1,8 +1,9 @@
 package repo
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
-	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -24,64 +25,35 @@ func Status(repoId, ref string) (*RepoStatus, error) {
 	}
 	dir := filepath.Join(config.RepoDir, repoId)
 
-	gitBin := gitBinPath()
-
-	cmd := exec.Cmd{
-		Path: gitBin,
-		Dir:  dir,
-		Args: []string{"git", "show-ref", ref},
-	}
-	if config.Debug {
-		cmd.Stderr = os.Stdout
-	}
-	outputBytes, err := cmd.Output()
-	if len(outputBytes) == 0 {
-		return nil, fmt.Errorf("Ref `%s` not found", ref)
-	}
-	if err != nil {
-		return nil, err
-	}
-	output := string(outputBytes)
-	lines := strings.Split(output, "\n")
-	commits := make([]string, 0, len(lines))
-	refs := make([]string, 0, len(lines))
-	for _, line := range lines {
-		parts := strings.SplitN(line, " ", 2)
-		if len(parts) != 2 {
-			continue
+	isHash := guessIsCommitHash(ref)
+	commit := ref
+	canonicalRef := ""
+	if !isHash {
+		var err error
+		commit, canonicalRef, err = commitByRef(dir, ref)
+		if err != nil {
+			return nil, err
 		}
-		commits = append(commits, parts[0])
-		refs = append(refs, parts[1])
 	}
-	if len(refs) == 0 {
-		return nil, fmt.Errorf("Ref `%s` not found", ref)
-	}
-	if len(refs) > 1 {
-		return nil, fmt.Errorf("Ref `%s` refer to multiple refs %v at %v", ref, refs, commits)
-	}
-
-	commit := commits[0]
-	canonicalRef := refs[0]
 
 	status := &RepoStatus{Commit: commit, Ref: canonicalRef}
 
-	cmd = exec.Cmd{
-		Path: gitBin,
+	var stdoutBuffer bytes.Buffer
+	cmd := exec.Cmd{
+		Path: gitBinPath(),
 		Dir:  dir,
 		Args: []string{"git", "show", "-q", "--pretty=format:%aI%n%cn <%ce>%n%s", commit}, // iso date, author, message
 	}
-	if config.Debug {
-		cmd.Stderr = os.Stdout
-	}
-	outputBytes, err = cmd.Output()
-	if len(outputBytes) == 0 {
+	gitDebug2(&cmd, &stdoutBuffer)
+	err := cmd.Run()
+	if stdoutBuffer.Len() == 0 {
 		return status, fmt.Errorf("Git show `%s` returned no output", commit)
 	}
 	if err != nil {
 		return status, err
 	}
-	output = string(outputBytes)
-	lines = strings.Split(output, "\n")
+	output := stdoutBuffer.String()
+	lines := strings.Split(output, "\n")
 	date := ""
 	if len(lines) > 0 {
 		date = lines[0]
@@ -95,4 +67,54 @@ func Status(repoId, ref string) (*RepoStatus, error) {
 		subject = lines[2]
 	}
 	return &RepoStatus{Commit: commit, Ref: canonicalRef, Author: author, Date: date, Subject: subject}, nil
+}
+
+func guessIsCommitHash(ref string) bool {
+	expected := hex.DecodedLen(len(ref))
+	if expected != 20 {
+		return false
+	}
+	dst := make([]byte, expected)
+	decoded, err := hex.Decode(dst, []byte(ref))
+	if err == nil && decoded == 20 {
+		return true
+	}
+	return false
+}
+
+func commitByRef(dir, ref string) (string, string, error) {
+	var stdoutBuffer bytes.Buffer
+	cmd := exec.Cmd{
+		Path: gitBinPath(),
+		Dir:  dir,
+		Args: []string{"git", "show-ref", ref},
+	}
+	gitDebug2(&cmd, &stdoutBuffer)
+	err := cmd.Run()
+	if stdoutBuffer.Len() == 0 {
+		return "", "", fmt.Errorf("Ref `%s` not found", ref)
+	}
+	if err != nil {
+		return "", "", err
+	}
+	output := stdoutBuffer.String()
+	lines := strings.Split(output, "\n")
+	commits := make([]string, 0, len(lines))
+	refs := make([]string, 0, len(lines))
+	for _, line := range lines {
+		parts := strings.SplitN(line, " ", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		commits = append(commits, parts[0])
+		refs = append(refs, parts[1])
+	}
+	if len(refs) == 0 {
+		return "", "", fmt.Errorf("Ref `%s` not found", ref)
+	}
+	if len(refs) > 1 {
+		return "", "", fmt.Errorf("Ref `%s` refer to multiple refs %v at %v", ref, refs, commits)
+	}
+
+	return commits[0], refs[0], nil
 }
