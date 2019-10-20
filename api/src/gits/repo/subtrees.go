9a42907618d139e102a804509a1967e176dc99ea
Arkadi Shishlov <arkadi@agilestacks.com>
2019-10-21 01:47:07 +0300

    Git subtree split prefix

--- api/src/gits/repo/subtrees.go
+++ api/src/gits/repo/subtrees.go
@@ -14,10 +14,17 @@ import (
 )
 
 type AddSubtree struct {
-	Prefix     string
-	Remote     string
-	Ref        string
-	Squash     bool
+	Prefix      string
+	Remote      string
+	Ref         string
+	SplitPrefix string `json:"splitPrefix"`
+	splitBranch string
+	Squash      bool
+}
+
+type RemoteWithRef struct {
+	Remote string
+	Ref    string
 }
 
 // TODO add subtree to an empty branch
@@ -46,9 +53,10 @@ func AddSubtrees(repoId, branch string, subtrees []AddSubtree) error {
 	}
 	defer deleteDir(clone)
 
+	gitBin := gitBinPath()
 	// clone
 	cmd := exec.Cmd{
-		Path: gitBinPath(),
+		Path: gitBin,
 		Dir:  "/",
 		Args: []string{"git", "clone", "--branch", branch, "--single-branch", "--no-tags", dir, clone},
 	}
@@ -71,27 +79,109 @@ func AddSubtrees(repoId, branch string, subtrees []AddSubtree) error {
 		}
 	}
 
-	// add subtrees
+	// see if there are subtrees that requires splitting a prefix
+	subtreeRemotes := make(map[string]RemoteWithRef)
 	for _, subtree := range subtrees {
-		args := []string{"git", "subtree", "add", "--prefix=" + subtree.Prefix, subtree.Remote, subtree.Ref}
-		if subtree.Squash {
-			args = append(args, "--squash")
+		if subtree.SplitPrefix != "" {
+			key := fmt.Sprintf("%s|%s", subtree.Remote, subtree.Ref)
+			subtreeRemotes[key] = RemoteWithRef{subtree.Remote, subtree.Ref}
 		}
-		cmd = exec.Cmd{
-			Path: gitBinPath(),
+	}
+	// for each unique combination of such remote/ref, fetch the ref and checkout as local branch
+	remoteIndex := 0
+	splitBranchIndex := 0
+	for _, remote := range subtreeRemotes {
+		remoteName := fmt.Sprintf("remote-%d", remoteIndex)
+		remoteIndex++
+		// git remote add remote-0 git@github.com:agilestacks/components.git
+		args := []string{"git", "remote", "add", remoteName, remote.Remote}
+		cmd = exec.Cmd{Path: gitBin, Dir: clone, Args: args}
+		gitDebug(&cmd)
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("Unable to add remote `%s`: %v", maskAuth(remote.Remote), err)
+		}
+		// git fetch remote-0 distribution:_remote-0/distribution
+		args = []string{"git", "fetch", remoteName, fmt.Sprintf("%s:_%s/%[1]s", remote.Ref, remoteName)}
+		cmd = exec.Cmd{Path: gitBin, Dir: clone, Args: args}
+		gitDebug(&cmd)
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("Unable to fetch `%s` ref `%s`: %v", maskAuth(remote.Remote), remote.Ref, err)
+		}
+		// git checkout -b _remote-0-distribution _remote-0/distribution
+		args = []string{"git", "checkout", "-b", fmt.Sprintf("_%s-%s", remoteName, remote.Ref),
+			fmt.Sprintf("_%s/%s", remoteName, remote.Ref)}
+		cmd = exec.Cmd{Path: gitBin, Dir: clone, Args: args}
+		gitDebug(&cmd)
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("Unable to checkout `%s` ref `%s` as local branch: %v", maskAuth(remote.Remote), remote.Ref, err)
+		}
+		// now, for each subtree from remote/ref, split prefix into a branch
+		for i, subtree := range subtrees {
+			if subtree.SplitPrefix == "" || !(remote.Remote == subtree.Remote && remote.Ref == subtree.Ref) {
+				continue
+			}
+			splitBranchName := fmt.Sprintf("_split-%d", splitBranchIndex)
+			splitBranchIndex++
+			// git subtree split --prefix=pgweb -b _split-0
+			// TODO -q is too quiet
+			args = []string{"git", "subtree", "split", "-q", "--prefix=" + subtree.SplitPrefix, "-b", splitBranchName}
+			if subtree.Squash {
+				args = append(args, "--squash")
+			}
+			cmd = exec.Cmd{Path: gitBin, Dir: clone, Args: args}
+			gitDebug(&cmd)
+			err = cmd.Run()
+			if err != nil {
+				return fmt.Errorf("Unable to split `%s` ref `%s` prefix `%s` as local branch: %v",
+					maskAuth(remote.Remote), remote.Ref, subtree.SplitPrefix, err)
+			}
+			subtrees[i].splitBranch = splitBranchName
+		}
+	}
+
+	// return to the branch
+	if len(subtreeRemotes) > 0 {
+		cmd := exec.Cmd{
+			Path: gitBin,
 			Dir:  clone,
-			Args: args,
+			Args: []string{"git", "checkout", branch},
 		}
 		gitDebug(&cmd)
 		err = cmd.Run()
 		if err != nil {
-			return fmt.Errorf("Unable to add subtree `%s` ref `%s`: %v", maskAuth(subtree.Remote), subtree.Ref, err)
+			return fmt.Errorf("Unable to checkout `%s`: %v", branch, err)
+		}
+	}
+
+	// add subtrees
+	for _, subtree := range subtrees {
+		args := []string{"git", "subtree", "add", "--prefix=" + subtree.Prefix}
+		if subtree.SplitPrefix == "" {
+			args = append(args, subtree.Remote, subtree.Ref)
+		} else {
+			args = append(args, subtree.splitBranch)
+		}
+		if subtree.Squash {
+			args = append(args, "--squash")
+		}
+		cmd = exec.Cmd{Path: gitBin, Dir: clone, Args: args}
+		gitDebug(&cmd)
+		err = cmd.Run()
+		if err != nil {
+			prefix := ""
+			if subtree.SplitPrefix != "" {
+				prefix = fmt.Sprintf(" prefix `%s`", subtree.SplitPrefix)
+			}
+			return fmt.Errorf("Unable to add subtree `%s` ref `%s`%s: %v", maskAuth(subtree.Remote), subtree.Ref, prefix, err)
 		}
 	}
 
 	// push
 	cmd = exec.Cmd{
-		Path: gitBinPath(),
+		Path: gitBin,
 		Dir:  clone,
 		Args: []string{"git", "push"},
 	}
@@ -105,7 +195,11 @@ func AddSubtrees(repoId, branch string, subtrees []AddSubtree) error {
 		if config.Trace {
 			log.Printf("Subtrees added to `%s` repo:", repoId)
 			for _, subtree := range subtrees {
-				log.Printf("\t%s => %s @ %s", subtree.Prefix, maskAuth(subtree.Remote), subtree.Ref)
+				prefix := ""
+				if subtree.SplitPrefix != "" {
+					prefix = " /" + subtree.SplitPrefix
+				}
+				log.Printf("\t%s => %s%s @ %s", subtree.Prefix, maskAuth(subtree.Remote), prefix, subtree.Ref)
 			}
 		} else {
 			added := make([]string, 0, len(subtrees))
